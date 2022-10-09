90be6edfdf8dfdb58285cecfd638e739a680b504
Mert Alpaslan <m.alpaslan@azerion.com>
2022-10-09 14:05:08 +0300

    Add pagy gem for pagination

--- app/lib/reqres/users.rb
+++ app/lib/reqres/users.rb
@@ -1,6 +1,6 @@
 module Reqres
   module Users
-    def self.all(per_page: 12, page: 1)
+    def self.all(per_page: 6, page: 1)
       path = "users?per_page=#{per_page}&page=#{page}"
 
       Reqres::UserCollection.new call_api(path)
